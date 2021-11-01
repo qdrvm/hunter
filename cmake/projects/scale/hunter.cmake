8d1c26a17aafe8bdc4fe832eed0a228ce2902b4c
Harrm <abrehchs@gmail.com>
2021-11-01 12:51:14 +0300

    Update SCALE

--- cmake/projects/scale/hunter.cmake
+++ cmake/projects/scale/hunter.cmake
@@ -15,9 +15,9 @@ hunter_add_version(
     VERSION
     1.0.0
     URL
-    "https://github.com/soramitsu/scale-codec-cpp/archive/d464fa1af9b89d92203feed95d74e1485f38a4ac.tar.gz"
+    "https://github.com/soramitsu/scale-codec-cpp/archive/9195604038c4ba915b0809adc05cd1a3ea1ac41c.tar.gz"
     SHA1
-    df5394f26334edcbd2f0319d1105ad55420fa638
+    5077c500786250e23b9a833ba05e0a2764ef3403
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
