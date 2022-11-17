44c1c5461697bb109b670b42d19e83ad89b745b3
Alexander Lednev <57529355+iceseer@users.noreply.github.com>
2022-11-17 11:41:10 +0300

    Add erasure_coding_crust v0.0.1 (#75)

---
+++ cmake/projects/erasure_coding_crust/hunter.cmake
@@ -0,0 +1,27 @@
+# Copyright (c) 2016-2017, Alexander Lednev
+# Copyright (c) 2020, Soramitsu Co., Ltd.
+# All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+
+hunter_add_version(
+    PACKAGE_NAME
+    erasure_coding_crust
+    VERSION
+    0.0.1
+    URL https://github.com/soramitsu/erasure-coding-crust/archive/refs/tags/v0.0.1.zip
+    SHA1 a3ed370ab594726d7d1581ed1e10adf48365005d
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(erasure_coding_crust)
+hunter_download(PACKAGE_NAME erasure_coding_crust)
+
+
