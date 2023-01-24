0a9e5362f479eade21731f25b0a0a81160153e46
turuslan <turuslan.devbox@gmail.com>
2023-01-24 13:30:22 +0300

    erasure_coding_crust 0.0.2
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/projects/erasure_coding_crust/hunter.cmake
+++ cmake/projects/erasure_coding_crust/hunter.cmake
@@ -20,6 +20,15 @@ hunter_add_version(
     SHA1 a3ed370ab594726d7d1581ed1e10adf48365005d
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    erasure_coding_crust
+    VERSION
+    0.0.2
+    URL https://github.com/soramitsu/erasure-coding-crust/archive/refs/tags/v0.0.2.zip
+    SHA1 b5b860358236011c92a6c7f6134d12199f0a5818
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(erasure_coding_crust)
 hunter_download(PACKAGE_NAME erasure_coding_crust)
