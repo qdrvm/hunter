0360757f4d35b2574c54ccfbdda5a497d6e92723
Ruslan Tushov <turuslan@users.noreply.github.com>
2023-11-21 12:24:30 +0300

    qtils 0.0.2 (#112)
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/projects/qtils/hunter.cmake
+++ cmake/projects/qtils/hunter.cmake
@@ -10,6 +10,13 @@ hunter_add_version(
     SHA1 d65d41ea8e227fc9a93357850415aaefc71aca46
 )
 
+hunter_add_version(
+    PACKAGE_NAME qtils
+    VERSION 0.0.2
+    URL  https://github.com/qdrvm/qtils/archive/refs/tags/v0.0.2.zip
+    SHA1 cf06ed101bcfccc87a2c1fae8d0dab7d76ee0f41
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 
 hunter_cacheable(qtils)
