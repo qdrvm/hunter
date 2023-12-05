573ce3d0a6153464cd80e9376bb416ae3238b40a
turuslan <turuslan.devbox@gmail.com>
2023-12-05 17:31:22 +0300

    scale 1.1.1
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/projects/scale/hunter.cmake
+++ cmake/projects/scale/hunter.cmake
@@ -21,6 +21,12 @@ hunter_add_version(
     URL  https://github.com/soramitsu/scale-codec-cpp/archive/refs/tags/v1.1.0.tar.gz
     SHA1 ab95ac4126f27496942d328e96f2571b75e9378b)
 
+hunter_add_version(
+    PACKAGE_NAME scale
+    VERSION      1.1.1
+    URL  https://github.com/soramitsu/scale-codec-cpp/archive/refs/tags/v1.1.1.zip
+    SHA1 6bf8a0aacad91e03684ecebc7cb38b8cccdf769c)
+
 hunter_cmake_args(
     scale
     CMAKE_ARGS
