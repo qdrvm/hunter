475c0b5af5444bbdc4aae7ac7e69580a4faf4cf8
Ruslan Tushov <turuslan@users.noreply.github.com>
2024-02-22 14:51:15 +0300

    openssl and xxhash (#128)
    Co-authored-by: Harrm <abrehchs@gmail.com>

--- cmake/projects/xxhash/hunter.cmake
+++ cmake/projects/xxhash/hunter.cmake
@@ -12,13 +12,13 @@ hunter_add_version(
     PACKAGE_NAME
     xxhash
     VERSION
-    0.6.5-p0
+    0.8.2
     URL
-    "https://github.com/hunter-packages/xxHash/archive/0.6.5-p0.tar.gz"
+    "https://github.com/Cyan4973/xxHash/archive/v0.8.2.tar.gz"
     SHA1
-    7d84575a45360d5c5c075ceb3fc1eb6b334aa5a4
+    8df702581ebc48a5e05be0f1ede6e1e9139e4da1
 )
 
-hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_pick_scheme(DEFAULT url_sha1_make_only)
 hunter_cacheable(xxhash)
 hunter_download(PACKAGE_NAME xxhash)
