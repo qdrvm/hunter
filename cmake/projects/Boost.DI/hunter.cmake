924019595c0441e676ba4051dd7536d1ca433161
turuslan <turuslan.devbox@gmail.com>
2024-04-24 09:54:38 +0500

    update boost di
    - map mutex
    
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/projects/Boost.DI/hunter.cmake
+++ cmake/projects/Boost.DI/hunter.cmake
@@ -21,6 +21,13 @@ hunter_add_version(
     f6f59cb4cf3b586d6985e361ec762a307428ff29
 )
 
+hunter_add_version(
+    PACKAGE_NAME Boost.DI
+    VERSION 1.1.0-p1
+    URL  https://github.com/qdrvm/boost-di/archive/d5de6c9840c7fc2e44bf37134b4a14b88151ecc4.zip
+    SHA1 3dd986a80393e0ed64e7b0536d4d5a70c21fbb51
+)
+
 hunter_cmake_args(
     Boost.DI
 	CMAKE_ARGS 
