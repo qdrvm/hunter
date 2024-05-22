f7e047b26b7baece6dd195d1010149e606c48ec3
turuslan <turuslan.devbox@gmail.com>
2024-05-22 15:23:44 +0500

    dirty
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/projects/qtils/hunter.cmake
+++ cmake/projects/qtils/hunter.cmake
@@ -17,6 +17,13 @@ hunter_add_version(
     SHA1 cf06ed101bcfccc87a2c1fae8d0dab7d76ee0f41
 )
 
+hunter_add_version(
+    PACKAGE_NAME qtils
+    VERSION 0.0.2-dirty
+    URL  https://github.com/qdrvm/qtils/archive/bb5f440b4aa2b0bf66eacbb27ac97ddf64cd4e5e.zip
+    SHA1 574be4af9cfd79dc530583dc1fe78571b7afa662
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 
 hunter_cacheable(qtils)
