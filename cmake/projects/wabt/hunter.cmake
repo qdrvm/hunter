4d2c15ab56a16e424c284b7388ca2a7c2edb6f7d
turuslan <turuslan.devbox@gmail.com>
2024-06-14 15:37:10 +0500

    tag
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/projects/wabt/hunter.cmake
+++ cmake/projects/wabt/hunter.cmake
@@ -5,9 +5,9 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME wabt
-    VERSION latest
-    URL  https://github.com/qdrvm/wabt/archive/17951f2e4cb290f25c72c6ca03d4abae7caed0f4.zip
-    SHA1 2f50078d868d98032e4aa4c003d92b6ce796d0ef
+    VERSION 1.0.34-qdrvm2
+    URL  https://github.com/qdrvm/wabt/archive/refs/tags/1.0.34-qdrvm2.zip
+    SHA1 54d1f9db612c694f8101e3fee2722dd24e93f7c8
 )
 
 hunter_cmake_args(wabt CMAKE_ARGS
